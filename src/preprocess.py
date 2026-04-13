2781d0c95d6f655cfc4e70b2bfd5e439c85bb743
Ankit Raj <ankitraj765489@gmail.com>
2026-04-13 12:59:26 +0530

    Fixing deployment

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,19 +1,20 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import string
+from nltk.corpus import stopwords
+import nltk
 
-nltk.download('punkt')
+# only this is needed
 nltk.download('stopwords')
 
 def clean_text(text):
     text = text.lower()
-    tokens = word_tokenize(text)
+    
+    # simple tokenizer (NO punkt required)
+    words = text.split()
 
     words = [
-        word for word in tokens
-        if word not in stopwords.words('english')
-        and word not in string.punctuation
+        w for w in words
+        if w not in stopwords.words('english')
+        and w not in string.punctuation
     ]
 
     return " ".join(words)
